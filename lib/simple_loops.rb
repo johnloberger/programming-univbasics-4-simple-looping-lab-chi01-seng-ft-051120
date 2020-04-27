1844fd13a1a1aae90a43fffb024d067dc0104742
John Loberger <johnloberger@gmail.com>
2020-04-27 19:15:46 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -10,7 +10,7 @@ end
 
 def loop_message_n_times(message, number)
   counter = 0
-  message = "Hello Red Balloon."
+  message = "Hello Moon."
   while counter < number
     puts message
     counter += 1
