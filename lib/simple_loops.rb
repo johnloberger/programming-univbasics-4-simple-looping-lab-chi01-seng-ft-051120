e68e36ca3ed635578ebb40d0153de5348132669b
John Loberger <johnloberger@gmail.com>
2020-04-27 19:21:10 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,7 +1,6 @@
 # Write your methods here
 def loop_message_five_times(message)
   counter = 0
-  message = "Hello World."
   while counter < 5 do
     puts message
     counter += 1
@@ -10,11 +9,8 @@ end
 
 def loop_message_n_times(message, number)
   counter = 0
-  message = "Hello Moon."
   while counter < number
     puts message
     counter += 1
   end
 end
-
-loop_message_n_times("Hello Moon.", 5)
